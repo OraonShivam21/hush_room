3c48a3390142e1aad21452a73f3bdf4515ea80e3
oraonshivam21 <oraonshivam.0302@gmail.com>
2025-07-08 23:14:11 +0530

    using message routes in the server file

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.get("/api", (req, res) => {
   res.send("hello world! from hush_room api");
