73b0aae71f7a47aba1d02f87ceaf2774ddc01ee0
oraonshivam21 <oraonshivam.0302@gmail.com>
2025-07-08 23:13:34 +0530

    added methods to list users, get messages, and send message

---
+++ backend/src/controllers/message.controllers.js
@@ -0,0 +1,75 @@
+import cloudinary from "../lib/cloudinary";
+import { sendJsonResponse } from "../lib/helper";
+import Message from "../models/message.models";
+import User from "../models/user.models";
+
+export const listUsers = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    sendJsonResponse(res, 200, filteredUsers);
+  } catch (error) {
+    console.log("Error listing users:", error);
+    sendJsonResponse(res, 500, error);
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: receiverId } = req.params.id;
+    const senderId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        {
+          receiverId: receiverId,
+          senderId: senderId,
+        },
+        {
+          receiverId: senderId,
+          senderId: receiverId,
+        },
+      ],
+    });
+
+    sendJsonResponse(res, 200, messages);
+  } catch (error) {
+    console.log("Error getting messages:", error);
+    sendJsonResponse(res, 500, error);
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl = "";
+    if (image) {
+      // upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // realtime funcationlity
+
+    sendJsonResponse(res, 201, newMessage);
+  } catch (error) {
+    console.log("Error sending message:", error);
+    sendJsonResponse(res, 500, error);
+  }
+};
