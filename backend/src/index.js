5fc4f0dd8e872486357b35b9aba06c4238304b96
oraonshivam21 <oraonshivam.0302@gmail.com>
2025-07-08 22:46:41 +0530

    added cookie parser to read jwt token from cookies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -9,10 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-app.use("/api", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("hello world! from hush_room api");
 });
 
