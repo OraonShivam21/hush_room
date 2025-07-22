0942ee9f6bc12fd987e01e4c35b9ead73f3db1a3
oraonshivam21 <oraonshivam.0302@gmail.com>
2025-07-22 23:17:07 +0530

    added chat store

--- frontend/src/stores/chat.js
+++ frontend/src/stores/chat.js
@@ -1,5 +1,37 @@
 import { create } from "zustand";
+import { api } from "../lib/axios.js";
+import toast from "react-hot-toast";
 
 export const useChatStore = create((set, get) => ({
+  messages: [],
+  users: [],
   selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    try {
+      set({ isUsersLoading: true });
+      const res = await api.get("/message/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    try {
+      set({ isMessagesLoading: true });
+      const res = await get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
