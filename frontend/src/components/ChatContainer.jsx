05df02595d917f195cad58b90cb2d7992eb09127
oraonshivam21 <oraonshivam.0302@gmail.com>
2025-07-23 22:15:58 +0530

    working on chat container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,5 +1,35 @@
+import { useEffect } from "react";
+import { useChatStore } from "../stores/chat";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessagesLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">{messages}</div>
+
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
