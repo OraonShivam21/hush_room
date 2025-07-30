5985db82ee4349929c53218dc3671b228ab0e960
oraonshivam21 <oraonshivam.0302@gmail.com>
2025-07-30 23:37:57 +0530

    added socket connections for real time messages and user states & updated user dps

--- frontend/src/stores/chat.js
+++ frontend/src/stores/chat.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { api } from "../lib/axios.js";
 import toast from "react-hot-toast";
+import { useAuthStore } from "./auth.js";
 
 export const useChatStore = create((set, get) => ({
   messages: [],
@@ -34,7 +35,6 @@ export const useChatStore = create((set, get) => ({
   },
 
   sendMessage: async (data) => {
-    console.log("sendMessage store:", data);
     try {
       const { selectedUser, messages } = get();
       const res = await api.post(`/messages/send/${selectedUser._id}`, data);
@@ -43,6 +43,25 @@ export const useChatStore = create((set, get) => ({
       console.error("Error sending message", error);
       toast.error(error.response.data.message);
     }
+  },
+
+  subscribeToMessages: () => {
+    const { selectedUser } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+    socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser =
+        newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+
+      set({ messages: [...get().messages, newMessage] });
+    });
+  },
+
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
   },
 
   setSelectedUser: (selectedUser) => set({ selectedUser }),
