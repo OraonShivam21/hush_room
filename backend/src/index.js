13ae8e3a19fea038b7d6d35f5e2df61b4313ccae
oraonshivam21 <oraonshivam.0302@gmail.com>
2025-08-01 23:17:18 +0530

    updated backend to support frontend in prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { app, server } from "./lib/socket.js";
 import { connectDB } from "./lib/db.js";
@@ -11,6 +12,7 @@ import messageRoutes from "./routes/message.routes.js";
 dotenv.config();
 // const app = express(); // now created in socket.js file to create server for socket.io
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +29,14 @@ app.use("/api/messages", messageRoutes);
 app.get("/api", (req, res) => {
   res.send("hello world! from hush_room api");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // using the server created usng socket.io
 server.listen(PORT, () => {
