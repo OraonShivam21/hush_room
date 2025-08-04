a5312ac6944bb68f2a2074bb02ab96459a18cebb
Shivam Oraon <138677528+OraonShivam21@users.noreply.github.com>
2025-08-04 22:26:51 +0530

    updated secure for jwt

--- backend/src/lib/helper.js
+++ backend/src/lib/helper.js
@@ -14,7 +14,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, // prevents XSS attacks - cross-site scripting attacks
     sameSite: "strict", // prevents CSRF attacks - cross-site request forget attacks
-    secure: process.env.ENV === "PROD",
+    secure: process.env.NODE_ENV !== "development",
   });
 
   return token;
