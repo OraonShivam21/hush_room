7bb76b6203dbe170276177164669cfcd7faef475
OraonShivam21 <oraonshivam.0302@gmail.com>
2025-08-12 22:43:19 +0530

    updated missing code for json limit and cookie parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,8 +14,22 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+app.get("/api", (req, res) => {
+  res.send("hello world! from hush_room api");
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
